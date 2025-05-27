9435bbb53d4744bcf399507d06c78757098abdce
dianachn <mardianasiti853@gmail.com>
2025-05-27 10:19:33 +0000

    test page

---
+++ src/pages/Test.tsx
@@ -0,0 +1,14 @@
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+
+const Index = () => {
+  return (
+    <div className="min-h-screen bg-white">
+      <Navbar />
+      <div className="container mx-auto">contanet</div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Index;
